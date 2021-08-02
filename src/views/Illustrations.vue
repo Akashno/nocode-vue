10f9e6574bc1f68c2b31765a2cb9320a56f4a03a
Akash N O <noakash@ymail.com>
2021-08-02 20:40:35 +0530

    fix broken download links

--- src/views/Illustrations.vue
+++ src/views/Illustrations.vue
@@ -11,8 +11,8 @@
             <div class="md:col-start-2  md:col-span-full justify-center md:justify-start   flex flex-wrap  gap-5">
                 <div v-for="image in images" class="relative group w-32 h-auto md:w-40 " :key="image.id" >
                     <div class="buttons z-10 block  md:flex md:justify-between md:space-x-2 space-y-2 md:space-y-0 ">
-                        <a :href="image.svg" :download="image.svg" class="block  group-hover:opacity-100    opacity-0 bg-red-500 hover:bg-red-700 rounded-full text-white px-5 ">svg</a>
-                        <a :href="image.png" :download="image.png" class="block  group-hover:opacity-100    opacity-0 bg-green-500 hover:bg-green-700 rounded-full text-white px-5 ">png</a>
+                        <a :href="image.svg" :download="'https://nocodes.herokuapp.com'+image.svg" class="block  group-hover:opacity-100    opacity-0 bg-red-500 hover:bg-red-700 rounded-full text-white px-5 ">svg</a>
+                        <a :href="image.png" :download="'https://nocodes.herokuapp.com'+image.png" class="block  group-hover:opacity-100    opacity-0 bg-green-500 hover:bg-green-700 rounded-full text-white px-5 ">png</a>
                     </div>
                     <img  class="hover:opacity-90  " :src="'https://nocodes.herokuapp.com'+image.svg" alt="">
                 </div>
@@ -37,7 +37,6 @@ export default {
     async created(){
         const res = await fetch("https://nocodes.herokuapp.com/api/svgList/")
         const data = await res.json()
-        console.log(data)
         this.images = data
     }
 }
